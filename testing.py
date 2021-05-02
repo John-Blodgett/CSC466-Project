446ea2cc4fb77b454923c5184529ebdf54ddd5d3
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2021-05-01 18:27:35 -0700

    distribution function working

--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
 import win32com.client
 
+# returns a distribution of all the bytes in the file
+def get_file_distribution(file):
+    dists = [0 for i in range(256)]
+
+    with open(file, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            dists[byte[0]] += 1
+            byte = f.read(1)
+
+    return dists
+
 def get_file_metadata(path, filename, metadata):
     sh = win32com.client.gencache.EnsureDispatch('Shell.Application', 0)
     ns = sh.NameSpace(path)
