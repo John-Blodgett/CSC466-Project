cdd9cfa3550e35d1c3fe0c4b8b189e5782c0b51d
John-Blodgett <55813162+John-Blodgett@users.noreply.github.com>
2021-05-02 11:39:07 -0700

    get_file_metadata only needs path and filename

--- testing.py
+++ testing.py
@@ -13,6 +13,7 @@ def get_file_distribution(file):
     return dists
 
 def get_file_metadata(path, filename, metadata):
+    metadata = ['Name', 'Size', 'Item type', 'Date modified', 'Date created']
     sh = win32com.client.gencache.EnsureDispatch('Shell.Application', 0)
     ns = sh.NameSpace(path)
     file_metadata = dict()
@@ -26,5 +27,4 @@ def get_file_metadata(path, filename, metadata):
 if __name__ == '__main__':
     folder = r"C:\Users\johne\PycharmProjects\CSC466-Project\pdfs"
     filename = 'fish.jpg'
-    metadata = ['Name', 'Size', 'Item type', 'Date modified', 'Date created']
-    print(get_file_metadata(folder, filename, metadata))
+    print(get_file_metadata(folder, filename, ))
