4b9abf2efa134af538e644459f13cbaf2a4474f9
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2021-05-07 11:56:28 -0700

    added functionality for finding longest consective chain of same char

--- testing.py
+++ testing.py
@@ -2,13 +2,29 @@ import win32com.client
 import os.path
 
 # returns a distribution of all the bytes in the file
+# and the byte with the greatest consecutive count
 def get_file_distribution(file):
     dists = [0 for i in range(256)]
+    consecutive_byte = None
+    consecutive_count = 0
 
     with open(file, "rb") as f:
         byte = f.read(1)
+        curr_freq = 1
+        prev_byte = byte
+        
         while byte:
             dists[byte[0]] += 1
+
+            if prev_byte != byte:
+                if curr_freq > consecutive_count:
+                    consecutive_byte, consecutive_count = prev_byte, curr_freq
+
+                curr_freq = 0
+                prev_byte = byte
+
+            curr_freq += 1
+            
             byte = f.read(1)
 
     return dists
