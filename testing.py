c5394a182f8891e074b11bbe9d2970fbc2af7d67
harshit0646 <harshitclg@gmail.com>
2021-05-09 04:16:21 +0530

    added data generation and pushed csv

--- testing.py
+++ testing.py
@@ -1,12 +1,15 @@
-import win32com.client
 import os.path
-
+import hashlib
+import os
+import glob
+import csv 
 # returns a distribution of all the bytes in the file
 # and the byte with the greatest consecutive count
-def get_file_distribution(file):
+def get_file_distribution( path,filename,):
     dists = [0 for i in range(256)]
     consecutive_byte = None
     consecutive_count = 0
+    file = filename
 
     with open(file, "rb") as f:
         byte = f.read(1)
@@ -30,10 +33,36 @@ def get_file_distribution(file):
     return dists, consecutive_byte, consecutive_count
 
 def get_file_metadata(path, filename):
-    fullPath = path + "\\" + filename
+    fullPath =  filename
     return os.path.getsize(fullPath) / 1000000, os.path.splitext(fullPath)[1]
 
+def dataGeneration():
+    fields = ['filename', 'file size', 'file type', 'file dist of bytes', 'consecutiveByte', 'consecutiveCount' ,'SHA-256', 'SHA-1', 'MD5', 'SHA-3-256']
+    file = open("pdfHash.csv", "w")
+    csvwriter = csv.writer(file)
+    csvwriter.writerow(fields)
+    path = r"/mnt/f/pdfs/"
+    print(path)
+    count = 0
+    for filename in glob.glob(os.path.join(path, '*.pdf')):
+        with open(os.path.join(os.getcwd(), filename), 'rb') as f:
+            sha3 = hashlib.sha3_256(f.read()).hexdigest()
+            md5 = hashlib.md5(f.read()).hexdigest()
+            sha2 = hashlib.sha256(f.read()).hexdigest()
+            sha1 = hashlib.sha1(f.read()).hexdigest()
+            bytedist, consecutiveByte, consecutiveCount = get_file_distribution(path, filename)
+            fileSize, fileType = get_file_metadata(path, filename)
+            row = [filename, fileSize, fileType, str(bytedist), consecutiveByte, str(consecutiveCount), sha2, sha1, md5, sha3]
+            # print(row)
+            print(count)
+            count += 1
+            csvwriter.writerow(row)
+        # break
+
 if __name__ == '__main__':
-    path = r"C:\Users\johne\PycharmProjects\CSC466-Project\images"
-    filename = 'fish.jpg'
-    print(get_file_metadata(path, filename))
+    # path = r"/mnt/f/pdfs/"
+    # filename = '5-Level Paging and 5-Level EPT - Intel - Revision 1.0 (December, 2016).pdf'
+    # # print(get_file_metadata(path, filename))
+    # a,b,c= (get_file_distribution(path, filename))
+    # print(str(a))
+    dataGeneration()
